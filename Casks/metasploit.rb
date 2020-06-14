51ea62e2d79e9075a0c60f7cc59365aa5c5c67ad
Ronald <haksolo@Ronalds-MacBook-Pro.local>
2020-06-14 21:06:43 +0800

    initial commit; added metasploit

---
+++ Casks/metasploit.rb
@@ -0,0 +1,22 @@
+cask 'metasploit' do
+  version '5.0.94,20200614103432'
+  sha256 '97ce221489e427bac3e16c5141647aa697544398890915618b619aed9f3c9497'
+
+  url "https://osx.metasploit.com/metasploit-framework-#{version.before_comma}+#{version.after_comma}-1rapid7-1.pkg"
+  appcast 'https://osx.metasploit.com/LATEST'
+  name 'Metasploit Framework'
+  homepage 'https://www.metasploit.com/'
+
+  depends_on formula: 'nmap'
+
+  pkg "metasploit-framework-#{version.before_comma} #{version.after_comma}-1rapid7-1.pkg"
+
+  uninstall script: {
+                      executable: '/opt/metasploit-framework/bin/msfremove',
+                      input:      ['y'],
+                      sudo:       true,
+                    },
+            rmdir:  '/opt/metasploit-framework'
+
+  zap trash: '~/.msf4'
+end
